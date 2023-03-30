6863da609dff1a74a5292d9ce12aad691a5435ee
ekaterynakravchuk <77169152+ekaterynakravchuk@users.noreply.github.com>
2023-03-30 16:03:31 +0300

    add animation to menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,17 @@
-gsap.to(".menu__left", 
-  { rotation: 360,
-    duration: 2,
-    ease: "bounce.out"
-  }
-);
+let menuBtn = document.querySelector(".menu__btn");
+let menuRight = document.querySelector(".menu__right");
+let menuLeft = document.querySelector(".menu__left");
+menuBtn.addEventListener("click", function () {
+  menuLeft.classList.toggle("open");
+  menuRight.classList.toggle("open");
+  gsap.fromTo(
+    ".menu__left.open",
+    { y: "-100%", duration: 0.5, ease: "power4.out" },
+    { y: "0%" }
+  );
+  gsap.fromTo(
+    ".menu__right.open",
+    { y: "100%", duration: 0.5, ease: "power4.out" },
+    { y: "0%" }
+  );
+});
